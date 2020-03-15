c74027f669bae4bff9ef04f415a1011a7d120351
cyrusrefahi@gmail.com <cyrusrefahi@gmail.com>
2020-03-15 12:54:36 +0330

    Initial commit

---
+++ model/question.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const questionSchema = mongoose.Schema({
+
+    questionId: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    firstQuestion:{
+        type:String,
+        required:String
+    },
+    secondQuestion:{
+        type:String,
+        required:String,
+    },
+    bundleVersion:{
+        type:Number,
+        default:-1,
+    },
+    firstQuestionVoteNumber: {
+        type:Number,
+        default:0,
+    },
+    secondQuestionVoteNumber: {
+        type:Number,
+        default:0,
+    },
+    created: {
+        type: Date,
+        default:Date.now,
+    },
+    isVerified: {
+        type:Boolean,
+        default:false,
+    }
+},{timestamp:true})
+
+const Question = mongoose.model('Question' , questionSchema)
+
+module.exports = {Question}
