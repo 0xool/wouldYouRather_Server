c31d4f4394845709d50578e529730f199de96115
cyrusrefahi@gmail.com <cyrusrefahi@gmail.com>
2020-04-25 12:13:37 +0430

    changed getBatchQuestionUpdate to post

--- src/server.js
+++ src/server.js
@@ -278,7 +278,7 @@ app.get('/api/getAllVerifiedQuestion' , (req,res) => {
 //=====================================================================
 app.post('/api/getBatchQuestionUpdate' , (req,res) => {
     console.log(req.body.questionBatch)
-    var arr = JSON.parse(req.body.questionBatch);
+    var arr = req.body.questionBatch;
     var arrayToUpdate = []
     for (q of arr){
         arrayToUpdate.push(q.questionId)
